f7cf2bc8710456a7d4be75d96b8d63290c143c03
camcgreen <c.c.green@outlook.com>
2023-09-21 19:03:14 +0100

    Add services section

--- app/components/services.jsx
+++ app/components/services.jsx
@@ -3,29 +3,25 @@ import React from 'react';
 const Services = () => {
     return (
         <div className='card services' id='services'>
-            <div className='services__left'>
-                <h2>
-                    In the room
-                    <br />
-                    with you.
-                </h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-                    et massa mi. Aliquam in hendrerit urna. Pellentesque sit
-                    amet sapien fringilla, mattis ligula consectetur, ultrices
-                    mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet
-                    augue.{' '}
-                </p>
-                <p>
-                    Vestibulum auctor ornare leo, non suscipit magna interdum
-                    eu. Curabitur pellentesque nibh nibh, at maximus ante
-                    fermentum sit amet. Pellentesque commodo lacus at sodales
-                    sodales. Quisque sagittis orci ut diam condimentum, vel
-                    euismod erat placerat. In iaculis arcu eros, eget tempus
-                    orci facilisis id.
-                </p>
-            </div>
+            <h2>
+                In the room
+                <br />
+                with you.
+            </h2>
             <img src='/images/chair.jpg' alt='In the room with you.' />
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et
+                massa mi. Aliquam in hendrerit urna. Pellentesque sit amet
+                sapien fringilla, mattis ligula consectetur, ultrices mauris.
+                Maecenas vitae mattis tellus. Nullam quis imperdiet augue.{' '}
+            </p>
+            <p>
+                Vestibulum auctor ornare leo, non suscipit magna interdum eu.
+                Curabitur pellentesque nibh nibh, at maximus ante fermentum sit
+                amet. Pellentesque commodo lacus at sodales sodales. Quisque
+                sagittis orci ut diam condimentum, vel euismod erat placerat. In
+                iaculis arcu eros, eget tempus orci facilisis id.
+            </p>
         </div>
     );
 };
