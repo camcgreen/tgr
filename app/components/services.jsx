322fb1f8a272d79c9b37ab5fc2e4106af8f54a57
camcgreen <c.c.green@outlook.com>
2023-09-23 17:19:51 +0100

    Add another p

--- app/components/services.jsx
+++ app/components/services.jsx
@@ -29,6 +29,13 @@ const Services = () => {
                 sagittis orci ut diam condimentum, vel euismod erat placerat. In
                 iaculis arcu eros, eget tempus orci facilisis id.
             </p>
+            <p>
+                Vestibulum auctor ornare leo, non suscipit magna interdum eu.
+                Curabitur pellentesque nibh nibh, at maximus ante fermentum sit
+                amet. Pellentesque commodo lacus at sodales sodales. Quisque
+                sagittis orci ut diam condimentum, vel euismod erat placerat. In
+                iaculis arcu eros, eget tempus orci facilisis id.
+            </p>
         </div>
     );
 };
